2779db12c1849a150f75c640e58f8f607f61e3bc
Maysa Horita <Maysa.Horita@sprint.com>
2018-09-16 11:56:09 -0300

    Changes

--- server/server.js
+++ server/server.js
@@ -6,6 +6,8 @@ let {mongoose} = require('./db/mogoose');
 let {Todo} = require('./models/todo');
 let {User} = require('./models/user');
 
+const port = process.env.PORT || 3000;
+
 let app = express();
 
 app.use(bodyParser.json());
@@ -48,8 +50,8 @@ app.get('/todos/:id', (req, res) => {
 })
 
 
-app.listen(3000, () => {
-    console.log('Started on port 3000');
+app.listen(port, () => {
+    console.log(`Started on port ${port}`);
 });
 
 module.exports = {app};
