c07aeb493708c2a1a55771e5de217b1ecd7d8da5
Maysa Horita <Maysa.Horita@sprint.com>
2018-10-19 13:51:36 -0300

    Add POST /users route and token method

--- server/server.js
+++ server/server.js
@@ -84,17 +84,41 @@ app.patch('/todos/:id', (req, res) => {
         body.completedAt = null;
     }
 
-    Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then(todo => {
+    Todo.findByIdAndUpdate(id, { $set: body }, { new: true }).then(todo => {
         if (!todo) {
             return res.status(404).send()
         }
 
-        res.send({todo});
+        res.send({ todo });
     }).catch(e => {
         res.status(400).send();
     })
 });
 
+app.post('/users/', (req, res) => {
+    let body = _.pick(req.body, ['email', 'password']);
+    let user = new User(body);
+
+    user.save().then(() => {
+        return user.generateAuthToken();
+    }).then(token => {        
+        res.header('x-auth', token).send(user);
+    }).catch(e => {
+        res.status(400).send(e);
+    })
+});
+
+
+// app.post('/todos', (req, res) => {
+//     let todo = new Todo({
+//         text: req.body.text
+//     });
+//     todo.save().then((doc) => {
+//         res.send(doc);
+//     }, (e) => {
+//         res.status(400).send(e);
+//     });
+// });
 
 app.listen(port, () => {
     console.log(`Started on port ${port}`);
